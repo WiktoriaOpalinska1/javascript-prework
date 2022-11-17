1dee91c6604d44dcf23243a7b171798f7d457d8c
Wiktoria Opalińka <wopalinska.work@gmail.com>
2022-11-17 21:29:01 +0100

    Create simple .js code with play moves

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  printMessage('Nieprawidłowa wartość.');
+  playerMove = 'kamień';
+}
+printMessage('Twój ruch: ' + playerMove);
